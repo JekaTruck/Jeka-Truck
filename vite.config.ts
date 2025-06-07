9430cc3f39a0d7f6b88c8afcf5d1fef0c8128267
GabrielGrigorio <bielxavierg3@gmail.com>
2025-06-06 23:05:40 -0300

    Primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Jeka-Truck/', // Adicionado para corrigir caminhos no GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
